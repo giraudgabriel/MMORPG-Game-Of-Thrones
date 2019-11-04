825302620fc5823a3e78a0c1fb03c10bf3b57468
giraudgabriel <gabriel.giraud@fatec.sp.gov.br>
2019-11-03 21:29:59 -0300

    #FEATURE: Validando o cadastro do rpg.

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,27 @@
 module.exports.cadastro = (application, req, res) => {
-    res.render('cadastro')
+    res.render('cadastro', {
+        validacao: [],
+        dados: {}
+    })
+}
+
+module.exports.cadastrar = (application, req, res) => {
+
+    let dados = req.body
+
+    req.assert('nome', 'Nome não pode estar vazio').notEmpty()
+    req.assert('usuario', 'Usuário não pode estar vazio').notEmpty()
+    req.assert('senha', 'Senha não pode estar vazio').notEmpty()
+    req.assert('casa', 'Casa não pode estar vazio').notEmpty()
+
+    let erros = req.validationErrors()
+
+    if (erros) {
+        res.render('cadastro', {
+            validacao: erros,
+            dados : dados
+        })
+        return
+    }
+    res.send('Podemos cadastrar')
 }
--- app/routes/cadastro.js
+++ app/routes/cadastro.js
@@ -1,3 +1,5 @@
 module.exports = function (application) {
-	application.get('/cadastro', (req, res) => application.app.controllers.cadastro(application, req, res))
+	application.get('/cadastro', (req, res) => application.app.controllers.cadastro.cadastro(application, req, res))
+
+	application.post('/cadastrar', (req, res) => application.app.controllers.cadastro.cadastrar(application, req, res))
 }
