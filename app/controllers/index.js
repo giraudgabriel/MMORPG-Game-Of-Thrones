d4c97c660ac4878e8113b561eba5d0e35900f6f5
giraudgabriel <gabriel.giraud@fatec.sp.gov.br>
2019-11-03 22:24:15 -0300

    #FEATURE: Iniciando a configuração do express-session

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,25 @@
 module.exports.index = (application, req, res) => {
-    res.render('index')
+    res.render('index', {
+        validacao: [],
+        dados: {}
+    })
+}
+
+module.exports.autenticar = (application, req, res) => {
+
+    let dados = req.body
+
+    req.assert('usuario', 'Usuário não pode estar vazio').notEmpty()
+    req.assert('senha', 'Senha não pode estar vazio').notEmpty()
+
+    let erros = req.validationErrors()
+
+    if (erros) {
+        res.render('index', {
+            validacao: erros,
+            dados: dados
+        })
+        return
+    }
+    res.send("Sessão iniciada")
 }
