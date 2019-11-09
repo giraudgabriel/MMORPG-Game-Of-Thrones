57f6a6e5ed9b1b6e6ca624ae731706d8fe40b6c9
giraudgabriel <gabriel.giraud@fatec.sp.gov.br>
2019-11-09 01:11:03 -0300

    #FEATURE: Formulário de aldeoes.

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -5,19 +5,17 @@ module.exports.jogo = async (application, req, res) => {
             validacao: [{
                 msg: 'É necessário estar logado!'
             }],
-            jogo:{}
+            jogo: {}
         })
         return
     }
 
-    const connection = application.config.dbConnection
-    const JogoDAO = new application.app.models.JogoDAO(connection)
+    const JogoDAO = new application.app.models.JogoDAO(application.config.dbConnection)
 
-    var jogo = await JogoDAO.iniciaJogo(req.session.usuario)
-    
     res.render("jogo", {
         img_casa: req.session.casa,
-        jogo: jogo
+        jogo: await JogoDAO.iniciaJogo(req.session.usuario),
+        comando_invalido: req.query.comando_invalido == 'S' ? 'S' : 'N'
     })
 
 }
@@ -32,10 +30,60 @@ module.exports.sair = (application, req, res) => {
 }
 
 
-module.exports.suditos = (application, req,res) =>{
+module.exports.suditos = (application, req, res) => {
+    if (req.session.autorizado !== true) {
+        res.render("index", {
+            dados: {},
+            validacao: [{
+                msg: 'É necessário estar logado!'
+            }],
+            jogo: {}
+        })
+        return
+    }
     res.render("aldeoes")
 }
 
-module.exports.pergaminhos = (application, req,res) =>{
+module.exports.pergaminhos = (application, req, res) => {
+    if (req.session.autorizado !== true) {
+        res.render("index", {
+            dados: {},
+            validacao: [{
+                msg: 'É necessário estar logado!'
+            }],
+            jogo: {}
+        })
+        return
+    }
     res.render("pergaminhos")
+}
+
+module.exports.ordernar_acao_sudito = (application, req, res) => {
+    if (req.session.autorizado !== true) {
+        res.render("index", {
+            dados: {},
+            validacao: [{
+                msg: 'É necessário estar logado!'
+            }],
+            jogo: {}
+        })
+        return
+    }
+    
+    let dados = req.body
+
+    req.assert('acao', 'Ação deve ser informada').notEmpty()
+    req.assert('quantidade', 'Quantidade deve ser informada').notEmpty()
+
+
+    let erros = req.validationErrors()
+
+    if (erros) {
+        res.redirect('jogo?comando_invalido=S')
+        return
+    }
+
+    console.log(dados)
+
+    res.send('Tudo ok')
 }
--- app/routes/jogo.js
+++ app/routes/jogo.js
@@ -6,4 +6,7 @@ module.exports = function (application) {
 	application.get('/suditos', (req, res) => application.app.controllers.jogo.suditos(application, req, res))
 	
 	application.get('/pergaminhos', (req, res) => application.app.controllers.jogo.pergaminhos(application, req, res))
+	
+	application.post('/ordernar_acao_sudito', (req, res) => application.app.controllers.jogo.ordernar_acao_sudito(application, req, res))
+
 }
